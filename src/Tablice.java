16489ad687d337009d9cb77d1f049359836c3e79
matkasprzak <matkasprzak@gmail.com>
2017-07-12 20:30:15 +0200

    Initialize commit

---
+++ src/Tablice.java
@@ -0,0 +1,96 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+      /*  int[] tablica = new int[] {1,2,3,4,5,6,7,8,9,10};
+        int wynik = 0;
+
+
+        for (int elemen: tablica) {
+            System.out.println(elemen);
+            wynik += elemen;
+        }
+
+        System.out.println("Suma elementów to: " + wynik);*/
+      //wczytywanie i tworzenie tablicy
+      /*  Scanner scanner = new Scanner(System.in);
+        System.out.println("Wpisz liczbe od 1 do 30");
+        int liczba = scanner.nextInt();
+
+        if( liczba > 0 && liczba < 31) {
+            int[] tablica = new int[liczba];
+            for (int elem=0; elem < liczba; elem++) {
+                System.out.println("Podaj liczbę " +  elem);
+                tablica[elem] = scanner.nextInt();
+
+            }
+            for ( int elem: tablica) {
+                System.out.println("Liczby to" + elem);
+            }
+        }else
+            System.out.println("Nieprawidłowa liczba");*/
+//podnoszenie do kwadratu
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj ilość elementów");
+        int ilosc = scanner.nextInt();
+        float[] liczby = new float[ilosc];
+        for (int i = 0; i < ilosc; i++) {
+            System.out.println(" Podaj liczbe " + ( i + 1));
+            liczby[i] = scanner.nextFloat();
+        }
+        for (int i = 0; i < ilosc; i++) {
+            liczby[i]= liczby[i] * liczby[i];
+
+        } for (float elem: liczby) {
+            System.out.println(elem);
+        }
+        for (int i = 0; i < ilosc; i++) {
+            System.out.println( i + "Liczba to " + liczby[i]);*/
+        //}
+        //Wyszukiwanie linjowe najwiekszej liczby
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj ilość elementów");
+        int ilosc = scanner.nextInt();
+        float[] liczby = new float[ilosc];
+        float max = 0;
+        for (int i = 0; i < ilosc; i++) {
+            System.out.println("Podaj wartości");
+            liczby[i] = scanner.nextFloat();
+        }
+        for (float i :liczby) {
+            if( i > max ) {
+                max = i;
+            }
+        } System.out.println("Największa liczba to " + max);*/
+
+        //tablice 2 wymiarowe
+        /*int[][] macierz = new int [5][5];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj elementy macierzy");
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        for (int i = 0; i<macierz.length; i++) {//lub zmiena x
+            for (int j = 0; j < macierz[i].length; j++) {//lub zmiena y
+                macierz[i][j] = scanner.nextInt();
+            }
+
+        }for (int n = 0; n<3; n++) {
+            for (int m = 0; m<3; m++){
+                System.out.print(macierz[n][m] + " ");
+            }System.out.println();*/
+        }
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+
